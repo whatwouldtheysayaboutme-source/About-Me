7802aed98e068e7d768e166768491bd1a0a4f742
Bobby Britton <whatwouldtheysayaboutme@gmail.com>
2025-12-15 15:16:52 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -10,7 +10,6 @@
 
 
   // public/app.js
-const API_BASE = "https://about-me-api-9m4q.onrender.com";
 console.log("[AboutMe] app.js loaded. API_BASE =", API_BASE);
 
   // -----------------------------
